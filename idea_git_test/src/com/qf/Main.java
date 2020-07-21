6fcfcfeb5e779e5b915a81c05b94b39e5a76fd7b
lituanchuan <945992126@qq.com>
2020-07-21 22:08:51 +0800

    提交

---
+++ idea_git_test/src/com/qf/Main.java
@@ -0,0 +1,14 @@
+package com.qf;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
